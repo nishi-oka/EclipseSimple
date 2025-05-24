68551bd9720d4c14d316cd7a9ba81df98dfe9db2
nishi-oka <nishioka31415@gmail.com>
2025-05-24 13:17:19 +0900

    AddTest

---
+++ src/EclipseAndGithub/test.java
@@ -0,0 +1,16 @@
+package EclipseAndGithub;
+
+public class test {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		double c = 10;
+		float d = 10;
+		
+		System.out.println(a == b);
+		System.out.println(c == d);
+
+	}
+
+}
