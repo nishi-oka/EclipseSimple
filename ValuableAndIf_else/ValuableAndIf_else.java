47ed52bfc1d4ebddd82d98474d24da1e419151ac
nishi-oka <nishioka31415@gmail.com>
2025-05-24 20:02:48 +0900

    Add if-else

---
+++ ValuableAndIf_else/ValuableAndIf_else.java
@@ -0,0 +1,25 @@
+package ValuableAndIf_else;
+
+//【課題1】
+//動画内のサンプルプログラムをif-else文を使って書き直してみましょう。
+public class ValuableAndIf_else {
+	public static void main(String[] args) {
+		// args[0]で受け取ったIntegerをInt型にする
+		int price = Integer.parseInt(args[0]);
+		double rate = 0.10; //消費税率：10%
+		int discount, amount;
+
+		//priceの値が3000以上ならdiscountの値を300にする。
+		if (price >= 3000) {
+			discount = 300;
+		} else {
+			discount = 0;
+		}
+
+		//値引き後の金額×消費税込みの倍率を計算してからint型にキャスト変換し小数点以下を切り捨て、amountに代入。
+		amount = (int) ((price - discount) * (1 + rate));
+		System.out.println("値引金額：" + discount + "円");
+		System.out.println("税込金額：" + amount + "円");
+
+	}
+}
